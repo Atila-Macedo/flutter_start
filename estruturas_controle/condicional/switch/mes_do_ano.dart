8b3169eb1712d3dc6753a53e5de8771cec4b5ace
Atila-Macedo <dev.atila0110@hotmail.com>
2025-05-08 15:29:39 -0400

    atualização do switch com os meses do ano

--- estruturas_controle/condicional/switch/mes_do_ano.dart
+++ estruturas_controle/condicional/switch/mes_do_ano.dart
@@ -2,15 +2,20 @@
 import 'dart:math';
 
 void main() {
-  final int mes = Random().nextInt(12) + 1;
+  final int mes = Random().nextInt(9) + 1;
   print("Mês gerado: $mes");
   switch (mes) {
-    case 12:
-      print('Dezembro: Férias 🎉');
-      break;
-    case 1:
-      print('Janeiro: Ano Novo 🎆');
-      break;
+    case 12:print('Dezembro: Férias');break;
+    case 11:print('Novembro: Niver do Lívio');break;
+    case 10:print('Outubro: Niver do Naruto');break;
+    case 8:print('Agosto: Niver da Mamãe');break;
+    case 7:print('Julho: Meu Mês');break;
+    case 6:print('Junho: Festa Junina');break;
+    case 5:print('Maio: Mês do trabalhador');break;
+    case 4:print('Abril: Pascoa');break;
+    case 3:print('Março: Quaresma');break;
+    case 2:print('Fevereiro: Carnaval');break;
+    case 1:print('Janeiro: Ano Novo');break;
     default:
       print('Mês intermediário 📅');
   }
