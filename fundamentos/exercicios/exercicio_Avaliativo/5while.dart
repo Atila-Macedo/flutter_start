e2acf1a3ba25692288ba8f6ba535a8706b9b6340
Atila-Macedo <dev.atila0110@hotmail.com>
2025-05-10 14:42:56 -0400

    feat: Ajustes Finais no cáuculo de fatoriais

--- fundamentos/exercicios/exercicio_Avaliativo/5while.dart
+++ fundamentos/exercicios/exercicio_Avaliativo/5while.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 void main() {
   print('Digite um número para calcular o fatorial:');
   // int Final = int.parse(stdin.readLineSync()!);
-  int Final = 2;
+  int Final = 9;
   int fatorial = 1, inicial = 1;
   while (inicial <= Final) {
     fatorial *= inicial;
