1d7a7beb6cbc7188711fb60d4846fb8fecb7723a
Atila-Macedo <dev.atila0110@hotmail.com>
2025-05-09 23:36:18 -0400

    feat: Atribuição de novos exercícios

---
+++ fundamentos/exercicios/exercicio_Avaliativo/5while.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  print('Digite um número para calcular o fatorial:');
+  // int Final = int.parse(stdin.readLineSync()!);
+  int Final = 2;
+  int fatorial = 1, inicial = 1;
+  while (inicial <= Final) {
+    fatorial *= inicial;
+    inicial++;
+  }
+  print('O fatorial de $Final é $fatorial.');
+}
