467252f9d971a26e6c7e96f55bb0827fe7a47f61
Atila-Macedo <dev.atila0110@hotmail.com>
2025-05-31 15:49:29 -0400

    feat: correções de código

--- fundamentos/exercicios/03_tratamento_erros/04.dart
+++ fundamentos/exercicios/03_tratamento_erros/04.dart
@@ -1,5 +1,7 @@
 void main(){
+  // ignore: unused_local_variable
   final List<int> numeros = [1, 2, 3]; // Lista com 3 elementos
+  // ignore: unused_local_variable
   const int indiceInvalido = 10; // Índice inválido
   try{
     List<String>  nomes = ['Lucas', 'João'];
